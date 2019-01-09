798ea440a96fd9f0e19f12db2c8be6f62208c9f6
pamddimaano <pam.dimz@yahoo.com>
2019-01-09 23:13:00 +0800

    Initial Commit

--- polls/models.py
+++ polls/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-	question_text = models.CharField(max_length=100) 					#max length is 100 always
+	question_text = models.CharField(max_length=100) 											#max length is 100 always
 	pub_date = models.DateTimeField('date published')
 
 	def __str__(self):
 		return 'Question: {}'.format(self.question_text)
 
 
-class Choice(models.Model): 											#next line for readablility
-	question = models.ForeignKey(Question, on_delete=models.CASCADE) 	#Cascade is constant
+class Choice(models.Model): 																	#next line for readablility
+	question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name = 'choices') 	#Cascade is constant
 	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)								#default is optional parameter
+	votes = models.IntegerField(default=0)														#default is optional parameter
